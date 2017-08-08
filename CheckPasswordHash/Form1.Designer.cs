8559132a6ae01dd723119370814ae043bc281c9f
MikeS159 <madmike159@hotmail.co.uk>
2017-08-08 15:21:54 +0100

    Initial Commit
    Working for large hash files. Need to add code to use web API and allow multiple hashes to be entered.

---
+++ CheckPasswordHash/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace CheckPasswordHash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFile_Btn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.password_TB = new System.Windows.Forms.TextBox();
+            this.checkHash_Btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFile_Btn
+            // 
+            this.openFile_Btn.Location = new System.Drawing.Point(12, 12);
+            this.openFile_Btn.Name = "openFile_Btn";
+            this.openFile_Btn.Size = new System.Drawing.Size(75, 46);
+            this.openFile_Btn.TabIndex = 0;
+            this.openFile_Btn.Text = "Open Hash File";
+            this.openFile_Btn.UseVisualStyleBackColor = true;
+            this.openFile_Btn.Click += new System.EventHandler(this.openFile_Btn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // password_TB
+            // 
+            this.password_TB.Location = new System.Drawing.Point(12, 74);
+            this.password_TB.Name = "password_TB";
+            this.password_TB.PasswordChar = '*';
+            this.password_TB.Size = new System.Drawing.Size(260, 20);
+            this.password_TB.TabIndex = 1;
+            // 
+            // checkHash_Btn
+            // 
+            this.checkHash_Btn.Location = new System.Drawing.Point(12, 100);
+            this.checkHash_Btn.Name = "checkHash_Btn";
+            this.checkHash_Btn.Size = new System.Drawing.Size(75, 23);
+            this.checkHash_Btn.TabIndex = 2;
+            this.checkHash_Btn.Text = "Check Hash";
+            this.checkHash_Btn.UseVisualStyleBackColor = true;
+            this.checkHash_Btn.Click += new System.EventHandler(this.checkHash_Btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Lines Read = ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkHash_Btn);
+            this.Controls.Add(this.password_TB);
+            this.Controls.Add(this.openFile_Btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFile_Btn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox password_TB;
+        private System.Windows.Forms.Button checkHash_Btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CheckPasswordHash/Form1.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.VisualBasic.Devices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CheckPasswordHash
+{
+    public partial class Form1 : Form
+    {
+        List<string> hashesFromFile1 = new List<string>();
+        List<string> hashesFromFile2 = new List<string>();
+        StreamReader sr;
+        SHA1 hashFunction = new SHA1Managed();
+        string passwd;
+        byte[] passwordArray;
+        byte[] hashArray;
+        string hashToSearch;
+        string filePath;
+        bool threadSwitch = false;
+        bool doneReading = false;
+        bool stringFound = false;
+        ulong lineRead = 0;
+        ulong linesAtATime = 1000000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void readChunk()
+        {
+            if (!doneReading)
+            {
+                if (threadSwitch)
+                {
+                    hashesFromFile1.Clear();
+                    for (ulong i = 0; i < linesAtATime; i++)
+                    {
+                        if (sr.EndOfStream)
+                        {
+                            doneReading = true;
+                            break;
+                        }
+                        else
+                        {
+                            string s = sr.ReadLine();
+                            hashesFromFile1.Add(s);
+                        }
+                    }
+                }
+                else
+                {
+                    hashesFromFile2.Clear();
+                    for (ulong i = 0; i < linesAtATime; i++)
+                    {
+                        if (sr.EndOfStream)
+                        {
+                            doneReading = true;
+                            break;
+                        }
+                        else
+                        {
+                            string s = sr.ReadLine();
+                            hashesFromFile2.Add(s);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void searchChunk()
+        {
+            if (!threadSwitch)
+            {
+                if (hashesFromFile1.Contains(hashToSearch))
+                {
+                    stringFound = true;
+                }
+            }
+            else
+            {
+                if (hashesFromFile2.Contains(hashToSearch))
+                {
+                    stringFound = true;
+                }
+            }
+        }
+
+        private void masterSpawn()
+        {
+            doneReading = false;
+            stringFound = false;
+            sr = new StreamReader(filePath);
+            label2.Text = lineRead.ToString();
+
+            Thread t = new Thread(readChunk);
+            t.Start();
+            t.Join();
+
+            while ((!doneReading) && (!stringFound))
+            {                
+                lineRead = lineRead + linesAtATime;
+                label2.Text = lineRead.ToString();
+                Application.DoEvents();
+                threadSwitch = !threadSwitch;
+                Thread t1 = new Thread(readChunk);
+                Thread t2 = new Thread(searchChunk);
+                t1.Start();
+                t2.Start();
+                t1.Join();
+                t2.Join();
+            }
+            threadSwitch = !threadSwitch;
+            t = new Thread(searchChunk);
+            sr.Close();
+            lineRead = 0;
+            hashesFromFile1.Clear();
+            hashesFromFile2.Clear();
+            if (stringFound)
+            {
+                MessageBox.Show("Password found - change it where ever you used it and never use it again");
+            }
+            else
+            {
+                MessageBox.Show("Password not found- you are ok");
+            }
+        }
+
+        private void openFile_Btn_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = new DialogResult();
+            dr = openFileDialog1.ShowDialog();
+            if(dr == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                sr = new StreamReader(filePath);
+            }
+        }
+
+        private void checkHash_Btn_Click(object sender, EventArgs e)
+        {
+            passwd = password_TB.Text;
+            passwordArray = Encoding.UTF8.GetBytes(passwd);
+            hashArray = hashFunction.ComputeHash(passwordArray);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (byte b in hashArray)
+            {
+                stringBuilder.AppendFormat("{0:X2}", b);
+            }
+            hashToSearch = stringBuilder.ToString();
+            checkFreeMemory();
+            masterSpawn();
+        }
+
+        private void checkFreeMemory()
+        {
+            ComputerInfo CI = new ComputerInfo();
+            ulong mem = ulong.Parse(CI.AvailablePhysicalMemory.ToString());
+            linesAtATime = mem / (40 * 4 * 2 * 2);
+            //40 characters per hash * 4 bytes per char (worst case for UTF8 * 2 arrays at once * 2 so only 50% memory is used
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
